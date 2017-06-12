e067cc39c9ff6912f2cbbad9ceb1bb0651023cfe
harminder0640 <harminderrajla@gmail.com>
2017-06-12 03:47:49 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,14 +7,23 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  app.get('article-one', function (req, res){
+      res.send('article one is served here');
+  })
 });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+ app.get('article-two',function (req,res){
+     res.send('article two is served here');
+ }) 
 });
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  app.get('article-three',function (req,res){
+      res.send('article three is served here')
+  })
 });
 
 

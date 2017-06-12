745d9d972ec9003cbb758c9cf6287b6c7b91aeb1
harminder0640 <harminderrajla@gmail.com>
2017-06-12 04:24:00 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,26 +8,26 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
   app.get('article-one', function (req, res){
       res.send('article one is served here');
   
+});
+ app.get('article-two',function (req,res){
+     res.send('article two is served here');
+ });
+ 
+   app.get('article-three',function (req,res){
+      res.send('article three is served here');
 });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
- app.get('article-two',function (req,res){
-     res.send('article two is served here');
-  
-});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
   });
-  app.get('article-three',function (req,res){
-      res.send('article three is served here');
-  
-});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80

b1edb56c6dd7c1dd99e8b1475e3f73ab4fb85f2a
harminder0640 <harminderrajla@gmail.com>
2017-06-14 15:41:07 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.get('/', function (req, res) {
       // articleName == article-one
       // articles[articleName] == content object for article one
       var articleName = req.params.articleName;
-      res.send(createTemplate(articles[articlesName]));
+      res.send(createTemplate(articles[articleName]));
 
 //app.get('/article-one',function(req,res)
 //res.send(createTemplate(articleOne)))

daf6580da6965568cd012663ca1e975fbe128237
harminder0640 <harminderrajla@gmail.com>
2017-06-14 16:45:37 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,3 +16,21 @@ function moveRight(){
 img.onclick = function(){
 var interval = setInterval(moveRight, 100);
 };
+
+
+
+//Counter code
+var button = document.getElementaryById('counter');
+var counter = 0;
+
+button.onclick = function() {
+    
+      //Make a request to counter endpoint
+      
+      //Capture the response and store it in variable
+      
+      //Render the variable in correct span
+      counter = counter + 1;
+      var span = document.getElementaryById('count');
+      span.innerHTML = counter.toString();
+};

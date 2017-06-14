4987d4fd5d6e35f08b54a50ec47299ec9f3fb1d7
harminder0640 <harminderrajla@gmail.com>
2017-06-14 15:21:11 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,9 +85,10 @@ app.get('/', function (req, res) {
     //  res.send(createTemplate(articles[articlesName]));
 
 app.get('/article-one',function(req,res)
-res.send(createTemplate(articleOne)))      
-  
-});
+res.send(createTemplate(articleOne)
+
+
+));
 
 app.get('/article-two',function(req,res)
 res.send(createTemplate(articleTwo)))   
@@ -95,7 +96,7 @@ res.send(createTemplate(articleTwo)))
 });
 
 app.get('/article-three',function(req,res)
-res.send(createTemplate(articleThree));      
+res.send(createTemplate(articleThree)))      
   
 });
 

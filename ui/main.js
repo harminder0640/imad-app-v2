289dccf5aebee689444e1977687a83a97ece93ca
harminder0640 <harminderrajla@gmail.com>
2017-06-18 00:53:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,48 +1,32 @@
-console.log('Loaded!');
 
-//change the context of main-text div
-var element = document.getElementById('main-text');
-
-element.innerHTML='New value';
-
-// move the image
-var img=document.getElementById('madi');
-var marginleft=0;
-function moveRight(){
-    marginLeft=marginLeft +10;
-    img.style.marginLeft = marginLeft+'px';
-    
-}
-img.onclick = function(){
-var interval = setInterval(moveRight, 100);
-};
 
 
 
 //Counter code
-var button = document.getElementById('counter');
-
+var button = document.getElementById("counter");
+counter = 0;
 button.onclick = function() {
+    counter = counter +1;
+    
     
       //Create the request object
-      var request = new XMLHttpRequest();
+    //  var request = new XMLHttpRequest();
       
       //Capture the response and store it in variable
-      request.onreadyStatechange = function() {
-          if (request.readystate===XTMLHttpRequest.DONE) {
+      //request.onreadyStatechange = function() {
+        //  if (request.readystate===XTMLHttpRequest.DONE) {
             //Take some action
-            if(request.status === 200) {
-                var counter = request.responseText;
+          //  if(request.status === 200) {
+            //    var counter = request.responseText;
                  var span = document.getElementById('count');
-                  span.innerHTML = counter;
-            }
-          }
+                  span.innerHTML = counter.toString();
+         
           //Not done yet
-      };
+      
       
     //Make the request
-    request.open('GET','http://harminder0640.imad.hasura-app.io/counter',true);
-    request.send(null);
+  //  request.open('GET','http://harminder0640.imad.hasura-app.io/counter',true);
+    //request.send(null);
     
 };
 

6216c6c9479ecfded021dc30ca985bb952fa9074
harminder0640 <harminderrajla@gmail.com>
2017-06-18 03:51:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,12 @@
+
 //Counter code
 var button = document.getElementById("counter");
-counter = 0;
+//counter = 0;
 
-button.onclick = function(){
-    counter = counter +1;
+button.onclick = function() {
     
   
-var submit = document.getElementById('submit_btn');
-submit.onclick= function(){
-  };
+
       //Create the request object
       var request = new XMLHttpRequest();
       
@@ -17,16 +15,37 @@ submit.onclick= function(){
       if (request.readystate===XTMLHttpRequest.DONE) {
             //Take some action
       if(request.status === 200) {
-          var names = request.responseText;
+          var counter = request.responseText;
+          var span = document.getElementById('count');
+          span.innerHTML = counter.toString();
+      }
+      }
+      
+      };
+      
+       request.open('GET','http://harminder0640.imad.hasura-app.io/counter',true);
+   request.send(null);
+      
+      };
+      var submit = document.getElementById('submit_btn');
+submit.onclick= function(){
+   
+   var request = new XMLHttpRequest();
+    request.onreadyStatechange = function() {
+      if (request.readystate===XTMLHttpRequest.DONE) {
+            //Take some action
+      if(request.status === 200) {
+      
+      var names = request.responseText;
           names = JSON.parse(names);
   var list = '';
 for (var i=0; i<names.length; i++) {
-    list +='<li>' + '</li>';
+    list +='<li>' + names[i]'</li>';
 }
 var ul = document.getElementById('namelist');
 ul.innerHTML = list;
-       }
-      }
+       };
+      };
           //Not done yet
       };
       

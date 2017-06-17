5fcd6621328cd507a55d6e00fc310a5c31b30833
harminder0640 <harminderrajla@gmail.com>
2017-06-18 01:01:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool;
-var crypto = require('crypto');
-var config = {
-    user: 'harminder0640',
-    database: 'harminder0640',
-    host: 'db.imad.hasura-app.io',
-    port: '5432',
-    password: process.env.DB_PASSWORD
-};
 
 
 var app = express();
@@ -88,47 +79,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-function hash (input, salt) {
-    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512 ');
-    return hashed.toString('hex');
-    
-}
-app.get('/hash/:input' function(req, res) {
-    var hashedString = hash(req.params.input, 'this-is-some-random-string');
-    res.send(hashedString);
-    
-});
-var pool = new Pool(config);
- app.get('/test-db',function(req, res){
-     pool.query("SELECT = FROM test"), function(err, result) {
-         if(err)
-         {
-             res.status(500).send(err.toString());
-             
-         } 
-         else 
-             res.send(JSON.string(fy(result.rows)));
-             app.get('/articles/:articleName',function(req, res){
-                 pool.query("SELECTB*FROM article WHERE title='"+ req.params.articleName +"'",function(err,result){
-                     if(err)
-                     {
-                         res.status(500).send(err.toString());
-                         
-                     }else{
-                         if(result.rows.length===0)
-                         {
-                             res.status(404).send('article not found');
-                             }else{
-                                 var articleData=result.rows[0];
-                                 
-                                 res.send(createTemplate(articleData));
-                                 
-                              
-                             }
-                     }
-                 });
-        res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
         
    var counter = 0;
    app.get('/counter',function (req, res) {

2d43470a8e93c3b4d9d6498872a325b766b877e0
harminder0640 <harminderrajla@gmail.com>
2017-06-18 03:07:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,28 +5,35 @@
 //Counter code
 var button = document.getElementById("counter");
 counter = 0;
+
 button.onclick = function(){
     counter = counter +1;
     
     
       //Create the request object
-    //  var request = new XMLHttpRequest();
+      var request = new XMLHttpRequest();
       
       //Capture the response and store it in variable
-      //request.onreadyStatechange = function() {
-        //  if (request.readystate===XTMLHttpRequest.DONE) {
+      request.onreadyStatechange = function() {
+      if (request.readystate===XTMLHttpRequest.DONE) {
             //Take some action
-          //  if(request.status === 200) {
-            //    var counter = request.responseText;
-                 var span = document.getElementById("count");
-                  span.innerHTML = counter.toString();
-         
+      if(request.status === 200) {
+          var names = request.responseText;
+          names = JSON.parse(names);
+  var list = '';
+for (var i=0; i<names.length; i++) {
+    list +='<li>' + '</li>';
+}
+var ul = document.getElementById('namelist');
+ul.innerHTML = list;
+       }
+      }
           //Not done yet
-      
+      };
       
     //Make the request
-  //  request.open('GET','http://harminder0640.imad.hasura-app.io/counter',true);
-    //request.send(null);
+    request.open('GET','http://harminder0640.imad.hasura-app.io/submit-name?name=' + name,true);
+   request.send(null);
     
 };
 
@@ -35,12 +42,6 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick= function(){
   
-  var names = ['name1','name2', 'name3', 'name4'];  
-  var list = '';
-for (var i=0; i<names.length; i++) {
-    list +='<li>' + '</li>';
-}
-var ul = document.getElementById('namelist');
-ul.innerHTML = list;
+ 
 };
     

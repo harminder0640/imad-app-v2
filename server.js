57fe1616ba34bdd7bce5b8a8199eb3005a3753a8
harminder0640 <harminderrajla@gmail.com>
2017-06-18 01:42:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,134 +2,30 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
-
 var app = express();
 app.use(morgan('combined'));
-    
-    var articles = {
-        'article-one': {
-         title: 'Article One | Harminder Singh',
-         heading: 'Article One',
-         date: 'June 12,2017',
-         content: ` 
-             <p>
-                this is the article for my first article. this is the article for my first article.this is the article for my first article.
-            </p>
-             <p>
-                this is the article for my first article. this is the article for my first article.this is the article for my first article.
-            </p>
-             <p>
-                this is the article for my first article. this is the article for my first article.this is the article for my first article.
-            </p>`
-        },
-        'article-two': {title: 'Article Two | Harminder Singh',
-         heading: 'Article Two',
-         date: 'June 15,2017',
-         content: ` 
-             <p>
-                this is the article for my second article.
-            </p>`},
-        'article-three': {title: 'Article Three | Harminder Singh',
-         heading: 'Article Three',
-         date: 'June 20,2017',
-         content: ` 
-             <p>
-                this is the article for my three article.
-            </p>`}
-        };
-
-function createTemplate (data) {
-    var title = data.title;
-    var date = data.date;
-    var heading = data.heading;
-    var content = data.content;
-    var htmlTemplate = `
-               <html>
-        <head>
-            <title>
-                ${title}
-            </title>
-            <meta name="viewport" content="device-width,initial-scale=1" />
-             <link href="/ui/style.css" rel="stylesheet" />
-           
-        </head>
-        <body>
-            <div class="container">
-             <div>
-                <a href="/">Home</a>
-             </div>
-             <hr/>
-             <h3>
-                ${heading}
-              </h3>
-              <div>
-              ${date}
-              </div>
-              <div>
-                 ${content}
-              </div>
-            </div>
-        </body>
-        </html>
-        `;
-        return htmlTemplate;
-}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-        
-   var counter = 0;
-   app.get('/counter',function (req, res) {
-       counter= counter +1;
-       res.send(counter.toString());
-   });
-   
-       var names= [];
-        app.get('/submit-name',function(rq,res) { //URL: /submit-name?name=xxxxxx
-            //Get the name from the request
-            var name = req.params.name;
-            
-            names.push(name);
-            //JSON: Java Script Object Notation
-            res.send(JSON.stringify(name));
-        });
-
-    app.get('/:articleName', function (req, res){
-      // articleName == article-one
-      // articles[articleName] == content object for article one
-      var articleName = req.params.articleName;
-      res.send(createTemplate(articles[articleName]));
-      
-
-//app.get('/article-one',function(req,res)
-//res.send(createTemplate(articleOne)))
-// });
-
-//app.get('/article-two',function(req,res)
-//res.send(createTemplate(articleTwo)))
- // });
-
-//app.get('/article-three',function(req,res)
-//res.send(createTemplate(articleThree));      
-  
+var counter=0;
+app.get('/counter', function(req,res) {
+    counter = counter+1;
+    res.send(counter.toString());
 });
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
+app.get('/article-one', function (req,res) { 
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+}); 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-  });
-  
-
+});
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});  
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80

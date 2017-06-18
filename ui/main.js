5377f24d8c6c3e59272147f14a66b0bfbcbc065b
harminder0640 <harminderrajla@gmail.com>
2017-06-18 10:55:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,61 +1,29 @@
+//counter code
 
-//Counter code
 var button = document.getElementById("counter");
-//counter = 0;
 
-button.onclick = function() {
+
+counter = 0;
+button.onclick = function(){
     
+counter = counter+1;
+
+ //create a request object
+// var request = new XMLHttpRequest();
   
-
-      //Create the request object
-      var request = new XMLHttpRequest();
-      
-      //Capture the response and store it in variable
-      request.onreadyStatechange = function() {
-      if (request.readystate===XTMLHttpRequest.DONE) {
-            //Take some action
-      if(request.status === 200) {
-          var counter = request.responseText;
-          var span = document.getElementById('count');
-          span.innerHTML = counter.toString();
-      }
-      }
-      
-      };
-      
-       request.open('GET','http://harminder0640.imad.hasura-app.io/counter',true);
-   request.send(null);
-      
-      };
-      var submit = document.getElementById('submit_btn');
-submit.onclick= function(){
-   
-   var request = new XMLHttpRequest();
-    request.onreadyStatechange = function() {
-      if (request.readystate===XTMLHttpRequest.DONE) {
-            //Take some action
-      if(request.status === 200) {
-      
-      var names = request.responseText;
-          names = JSON.parse(names);
-  var list = '';
-for (var i=0; i<names.length; i++) {
-    list +='<li>' + names[i]'</li>';
-}
-var ul = document.getElementById('namelist');
-ul.innerHTML = list;
-       };
-      };
-          //Not done yet
-      };
-      
-    //Make the request
-      var nameInput = document.getElementById('name');
-     var name = nameInput.value;
-    request.open('GET','http://harminder0640.imad.hasura-app.io/submit-name?name=' + name,true);
-   request.send(null);
+ //capture the response
+// request.onreadystatechange = function(){
+  //  if(request.readystate === XMLHttpRequest.DONE){
+    //take action
+//if(request.status===500){
+     //   var counter = request.responseText;
+        var span = document.getElementById("count");
+        span.innerHTML = counter.toString();
     
-};
-
-
     
+ 
+    //take no action.wait.
+ };
+ //make the request
+// request.open('GET','http://ankit158.imad.hasura-app.io/counter',true);
+// request.send(null);

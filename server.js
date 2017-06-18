c04e287c9c4ce1565f80300f5f1ad697dafd9c14
harminder0640 <harminderrajla@gmail.com>
2017-06-18 12:02:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ var pool = new Pool(config);
      if (err) {
          res.status(500).send(err.toString());
      } else {
-         res.send(JSON.stringify(result));
+         res.send(JSON.stringify(result.rows));
      }   
      });
      

74afd97c80a1b77b56c5c521f0699f80c9a8eb4b
harminder0640 <harminderrajla@gmail.com>
2017-06-19 09:11:19 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -46,7 +46,7 @@ function loadLogin () {
     // Check if the user is already logged in
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.readyState === XMLHttpRequest) {
             if (request.status === 200) {
                 loadCommentForm(this.responseText);
             }
